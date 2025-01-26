02b9fa81164b7532e269ec92ae9acc84b6924416
Darko Glodaic <darko.glodjaic@gmail.com>
2025-01-26 22:35:35 +0100

    Update Header.jsx to import logo image and updated CSS styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import chefRobotLogo from "/images/chef-icon.png"
+
 export default function Header() {
     return (
         <header className="header">
-            <img src='images/chef-icon.png' alt="Chef Robot Icon"/>
+            <img src={chefRobotLogo} alt="Chef Robot Logo"/>
             <h1>Appetizing Inpsirations</h1>
         </header>
     )
